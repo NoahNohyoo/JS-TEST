ab7bed8361ea5182fe43940724c39af85e2e6b5c
noahNoh <cananohyoo@gmail.com>
2021-12-08 15:49:28 -0800

    add check value

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,17 @@
 "USE STRICT";
 
-console.log('HI');
-console.log('Bye');
+const id = document.querySelector("#id"),
+    psword = document.querySelector("#psword"),
+    loginBtn = document.querySelector("button");
+
+loginBtn.addEventListener("click", login);
+
+function login() {
+    const req = {
+        id: id.value,
+        psword: psword.value,
+    };
+
+    console.log(req);
+
+}

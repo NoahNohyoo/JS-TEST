8774b0770966ebe47a695209874ae733e7412a26
noahNoh <cananohyoo@gmail.com>
2021-12-08 17:00:58 -0800

    add api post login

--- app/app.js
+++ app/app.js
@@ -2,6 +2,7 @@
 
 // Module
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
 // Routing
@@ -11,6 +12,9 @@ const home = require('./src/routes/home');
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 app.use(express.static(`${__dirname}/src/public`));
+app.use(bodyParser.json());
+// fix for space or othere languages
+app.use(bodyParser.urlencoded({ extended: true}));
 
 app.use('/', home); // middleWare(use)
 

a4394306e05c45b35033ff89497a26be55ae422a
noahNoh <cananohyoo@gmail.com>
2021-12-08 17:16:33 -0800

    add restAPI login

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,5 +1,6 @@
 'USE STRICT';
 
+
 const output = {
     home: (req, res) => {
         res.render('home/index');
@@ -9,9 +10,29 @@ const output = {
     },
 };
 
+const users = {
+    id: ["gaga", "nana", "dada"],
+    psword: ["1234", "1234", "123456"],
+};
+
 const process = {
     login: (req, res) => {
-        console.log(req.body);
+        const id = req.body.id,
+            psword = req.body.psword;
+
+        if (users.id.includes(id)) {
+            const idx = users.id.indexOf(id);
+            if (users.psword[idx] === psword) {
+                return res.json({
+                    success: true,
+                });
+            }
+        }
+
+        return res.json({
+            success: false,
+            msg: "Failed Login",
+        });
     },
 };
 
